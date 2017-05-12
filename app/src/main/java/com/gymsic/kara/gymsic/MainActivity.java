b7c4226fe33a45293716729ea26d8e7ad50c44e4
premkamon-PC\premkamon <premkamon746@gmail.com>
2017-05-12 15:34:38 +0700

    main activity

--- app/src/main/java/com/gymsic/kara/gymsic/MainActivity.java
+++ app/src/main/java/com/gymsic/kara/gymsic/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
- 
+
                     }
                 });
             }
