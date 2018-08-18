15343489ca2a5abd9c3e6b61a3df454dc614e4ee
premkamon <premkamon746@gmail.com>
2018-08-18 11:02:57 +0700

    create new recoard fragment

--- app/src/main/java/com/gymsic/kara/gymsic/MainActivity.java
+++ app/src/main/java/com/gymsic/kara/gymsic/MainActivity.java
@@ -105,16 +105,6 @@ public class MainActivity extends AppCompatActivity
 
 
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     public void onListFragmentInteraction(Song item) {
